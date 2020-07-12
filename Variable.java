5b7414399384da01621e71704adcf5dc114aa7b0
KaterinaBesp <KaterinaBesp@users.noreply.github.com>
2020-07-12 21:31:26 +0300

    lesson1 number6

---
+++ Variable.java
@@ -0,0 +1,19 @@
+public class Variable {	
+    public static void main(String[] args) {
+    	 byte RAM = 8 ;
+    	 short TypeSys = 64;
+    	 int VersionWin = 1909;
+    	 long CodeP = 00100326007;
+    	 float CPUGHz = 2.4f;
+    	 double OSassembly = 18363.900d;
+    	 char CPU = 'i';
+    	 boolean C = true;
+    	System.out.println("Operative memory"+ "-" + RAM);
+    	System.out.println("Type System"+ "-" + TypeSys);
+    	System.out.println("Version Windows"+ "-" + VersionWin);
+    	System.out.println("Code of product "+ "-" + CodeP );
+        System.out.println("CPU"+ "-" + "Core" + CPU + "5" + "," + CPUGHz + "GHz" );
+        System.out.println("OS assembly"+ "-" + OSassembly );
+        System.out.println("System parameters is"+ "-" + C );
+    }
+}		
