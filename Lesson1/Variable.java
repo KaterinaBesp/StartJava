33d1b656c0e7b789845057e97566b67a3adf4e32
KaterinaBesp <KaterinaBesp@users.noreply.github.com>
2020-07-13 17:40:05 +0300

    Change data for Variable.java

--- Lesson1/Variable.java
+++ Lesson1/Variable.java
@@ -1,19 +1,19 @@
-public class Variable {	
+public class Variable { 
     public static void main(String[] args) {
-    	 byte ram = 8 ;
-    	 short typesys = 64;
-    	 int versionwin = 1909;
-    	 long codeprod = 00100326007;
-    	 float cpughz = 2.4f;
-    	 double osassembly = 18363.900d;
-    	 char cpu = 'i';
-    	 boolean C = true;
-    	System.out.println("Operative memory" + "-" + ram);
-    	System.out.println("Type System" + "-" + typesys);
-    	System.out.println("Version Windows" + "-" + versionwin);
-    	System.out.println("Code of product " + "-" + codeprod );
-        System.out.println("CPU" + "-" + "Core" + cpu + "5" + "," + cpughz + "GHz" );
-        System.out.println("OS assembly" + "-" + osassembly );
-        System.out.println("System parameters is" + "-" + C );
+        byte ram = 8 ;
+        short typesys = 64;
+        int versionwin = 1909;
+        long codeprod = 00100326007;
+        float cpughz = 2.4f;
+        double osassembly = 18363.900d;
+        char cpu = 'i';
+        boolean iscorrectparsis = true;
+        System.out.println("Operative memory" + "-" + ram);
+        System.out.println("Type System" + "-" + typesys);
+        System.out.println("Version Windows" + "-" + versionwin);
+        System.out.println("Code of product " + "-" + codeprod);
+        System.out.println("CPU" + "-" + "Core" + cpu + "5" + "," + cpughz + "GHz");
+        System.out.println("OS assembly" + "-" + osassembly);
+        System.out.println("System information is correct" + "-" + iscorrectparsis);
     }
-}		
+}   
