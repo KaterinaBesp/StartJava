f91a984f076f29c351d99b41ba82146af68d08c6
KaterinaBesp <KaterinaBesp@users.noreply.github.com>
2020-07-14 00:06:20 +0300

    Add class Cycle

---
+++ Lesson1/Cycle.java
@@ -0,0 +1,10 @@
+public class Cycle {	
+	public static void main(String[] args) {
+		//TODO: Выведите на консоль с помощью цикла for все числа от [0, 20]
+		int number = 0;
+		for (int i = 1; i < 21; i++) {
+			number = number + 1;
+			System.out.println(number);
+		}
+	}
+}	
