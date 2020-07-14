29a48c396fba43dc11b097d6058855789dd4b272
KaterinaBesp <KaterinaBesp@users.noreply.github.com>
2020-07-14 23:16:11 +0300

    Change data for ConditionalStatement

--- Lesson1/ConditionalStatement.java
+++ Lesson1/ConditionalStatement.java
@@ -1,36 +1,36 @@
 public class ConditionalStatement  {	
 	public static void main(String[] args) {
 		//TODO: ЕСЛИ(возраст > 20) {выведите какое-то сообщение}
-		int возраст = 25;
-		if (возраст > 20) {
-			System.out.println(возраст + "- возраст человека старше 20 лет");
+		int age = 25;
+		if (age > 20) {
+			System.out.println(age + "- возраст человека старше 20 лет");
 		}
 
 		//TODO: ЕСЛИ(мужскойПол) {выведите какое-то сообщение} ЕСЛИ(неМужскойПол) { выведите какое-то сообщение}
-		String пол = "мужскойПол";
-		boolean isAMen = пол == "мужскойПол";
-		if (isAMen == true) {
-			System.out.println(пол + "- выражение истино");
-		} if (isAMen == false) {
-			System.out.println(пол + "- ошибка");
+		String sex = "мужскойПол";
+		boolean isMen = sex == "мужскойПол";
+		if (isMen) {
+			System.out.println(sex + "- выражение истино");
+		} if (!isMen) {
+			System.out.println(sex + "- ошибка");
 		}
 
 		//TODO: ЕСЛИ(рост < 1.80) {выведите какое-то сообщение} ИНАЧЕ { выведите какое-то сообщение}
-		float рост = 1.70f;
-		if (рост < 1.80f) {
-			System.out.println(рост + "- рост человека ниже 1.80");
+		float height = 1.70f;
+		if (height < 1.80f) {
+			System.out.println(height + "- рост человека ниже 1.80");
 		} else {
-			System.out.println(рост + "- рост человека ниже 1.80");
+			System.out.println(height + "- рост человека ниже 1.80");
 		}
 
 		//TODO: ЕСЛИ(перваяБукваИмени == ‘M’) { выведите какое-то сообщение} ИНАЧЕ ЕСЛИ(перваяБукваИмени == ‘I’) { выведите какое-то сообщение} ИНАЧЕ {выведите какое-то сообщение}
-		String перваяБукваИмени = "M";
-		if (перваяБукваИмени == "M") {
-			System.out.println(перваяБукваИмени + "-первая буква имени M");
-		} else if (перваяБукваИмени == "I") {
-			System.out.println(перваяБукваИмени + "-первая буква имени I");
+		String firstLetterName = "M";
+		if (firstLetterName == "M") {
+			System.out.println(firstLetterName + "-первая буква имени M");
+		} else if (firstLetterName == "I") {
+			System.out.println(firstLetterName + "-первая буква имени I");
 		} else {
-			System.out.println(перваяБукваИмени + "- первая буква имени отличная от M и I");
+			System.out.println(firstLetterName + "- первая буква имени отличная от M и I");
 		}
 	}
 }	
--- Lesson1/Cycle.java
+++ Lesson1/Cycle.java
@@ -1,18 +1,33 @@
 public class Cycle {	
 	public static void main(String[] args) {
 		//TODO: Выведите на консоль с помощью цикла for все числа от [0, 20]
-		int number = 0;
-		for (int i = 1; i < 21; i++) {
-			number = number + 1;
-			System.out.println(number);
-		}
-		
-		//TODO: Выведите на консоль с помощью цикла while все числа от [6, -6] (шаг итерации равен 2)
-		int value = -6;
+		// int number = 0;
+		// for (int i = 1; i < 21; i++) {
+		// 	number = number + 1;
+		// 	System.out.println(number);
+		// }
+
+		// //TODO: Выведите на консоль с помощью цикла while все числа от [6, -6] (шаг итерации равен 2)
+		// int value = -6;
+		// int i = 2;
+		// while(value <= 6) {
+		// 	System.out.println(value);
+		// 	value = value + i - 1;
+		// }
+
+		//TODO: Подсчитайте с помощью цикла do-while сумму нечетных чисел от [10, 20], и выведите ее на консоль
 		int i = 2;
-		while(value <= 6) {
-			System.out.println(value);
-			value = value + i - 1;
-		}
+		int result = 1;
+		do {
+			result = result + i;
+			if (result % 2 == 0) {
+				System.out.ptintln("Число " + result +" является четным");
+			} else {
+				System.out.ptintln("Число " + result +" является нечетным");
+			}
+			i++;
+		} while (i <= 5);
+		 	
+		System.out.ptintln("1 + 2 + 3 + 4 + 5 =" + result);
 	}
 }	
