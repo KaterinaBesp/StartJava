f1f79763ab0226a72fb2f345bfcc201e49119a92
KaterinaBesp <KaterinaBesp@users.noreply.github.com>
2020-07-15 10:55:16 +0300

    Change data for Cycle

--- Lesson1/Cycle.java
+++ Lesson1/Cycle.java
@@ -1,33 +1,35 @@
 public class Cycle {	
 	public static void main(String[] args) {
 		//TODO: Выведите на консоль с помощью цикла for все числа от [0, 20]
-		// int number = 0;
-		// for (int i = 1; i < 21; i++) {
-		// 	number = number + 1;
-		// 	System.out.println(number);
-		// }
+		int number = 0;
+		for (int i = 1; i < 21; i++) {
+			number = number + 1;
+			System.out.println(number);
+		}
 
 		// //TODO: Выведите на консоль с помощью цикла while все числа от [6, -6] (шаг итерации равен 2)
-		// int value = -6;
-		// int i = 2;
-		// while(value <= 6) {
-		// 	System.out.println(value);
-		// 	value = value + i - 1;
-		// }
+		int value = -6;
+		int j = 2;
+		while(value <= 6) {
+			System.out.println(value);
+			value = value + j - 1;
+		}
 
 		//TODO: Подсчитайте с помощью цикла do-while сумму нечетных чисел от [10, 20], и выведите ее на консоль
-		int i = 2;
-		int result = 1;
+
+		int k = 2;
+		int sum =11; 
 		do {
-			result = result + i;
-			if (result % 2 == 0) {
-				System.out.ptintln("Число " + result +" является четным");
+			sum = sum + k;
+			if (sum % 2 != 0) {
+				System.out.println(sum + " число нечетное");
 			} else {
-				System.out.ptintln("Число " + result +" является нечетным");
+				System.out.println(sum + " число четное");
 			}
-			i++;
-		} while (i <= 5);
-		 	
-		System.out.ptintln("1 + 2 + 3 + 4 + 5 =" + result);
+			k++;
+		} while (k <= 5);
+
+		System.out.println("11 + 13 + 15 + 17 + 19 =" + sum);
 	}
+
 }	
