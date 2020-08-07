21e1ac292fd1379494a76d9fe547d3b9a13313b4
KaterinaBesp <KaterinaBesp@users.noreply.github.com>
2020-08-08 00:13:52 +0300

    change data for MyFirstGame

--- Lesson1/MyFirstGame.java
+++ Lesson1/MyFirstGame.java
@@ -1,15 +1,16 @@
 public class MyFirstGame {	
-	public static void main (String [] args) {
-		int randomNum = 77;
-		int computerNum = 74;
+	public static void main(String[] args) {
+		int rndNum = 77;
+		int compNum = 74;
 		do {
-			if (computerNum < randomNum) {
-				System.out.println ("Введенное вами число-" + randomNum + " больше того, что загадал компьютер:");
-				randomNum --;
-			} else if (computerNum > randomNum) {
-				System.out.println ("Введенное вами число-"+ randomNum + "меньше того, что загадал компьютер:");
-				randomNum ++; }
-			} while (randomNum != computerNum);
-		System.out.println ("Вы угадали! " + randomNum);
+			if (compNum < rndNum) {
+				System.out.println ("Введенное вами число-" + rndNum + " больше того, что загадал компьютер:");
+				rndNum--;
+			} else if (compNum > rndNum) {
+				System.out.println ("Введенное вами число-"+ rndNum + "меньше того, что загадал компьютер:");
+				rndNum++; 
+			}
+		} while (rndNum != compNum);
+		System.out.println ("Вы угадали! " + rndNum);
 	}	
 }
