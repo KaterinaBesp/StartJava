6704492738bbb25645ded15ac62d1b6dfda9d9a3
KaterinaBesp <KaterinaBesp@users.noreply.github.com>
2020-08-08 01:46:09 +0300

    Add class calculator

---
+++ Lesson1/Calculator.java
@@ -0,0 +1,32 @@
+public class Calculator {
+	public static void main(String[] args) {
+		int x = 5	;
+		int y = 3;
+		int result;
+		char sign = '^';
+
+		if(sign == '+') {
+			result = x + y;
+			System.out.println(" x + y = " + result);
+		} else if(sign == '-') {
+			result = x - y;
+			System.out.println(" x - y = " + result);
+		} else if(sign == '*') {
+			result = x * y;
+			System.out.println(" x * y = " + result);
+		} else if(sign == '/') {
+			result = x / y;
+			System.out.println(" x / y = " + result);
+		} else if (sign == '^') {
+			result = x;
+			while (y > 1) { 
+				result *=  x;
+				y--;
+			}
+			System.out.println(" x ^ y = " + result);
+		} else if(sign == '%') {
+			result = x % y;
+			System.out.println(" x % y = " + result);
+		}
+	}
+}
