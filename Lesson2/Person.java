3a24527e0622e4c7229162918b4322d84d15efdb
KaterinaBesp <KaterinaBesp@users.noreply.github.com>
2020-08-13 00:27:15 +0300

    Add person.java

---
+++ Lesson2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+	String namePerson = "Alex";
+	String sex = "Male";
+	float height = 1.86f;
+	float weight = 75f;
+	int age = 28;
+
+	boolean move () {
+		System.out.println("I want to move");
+		return true;
+	}
+
+	void sit () {
+		System.out.println("I want to sit down");
+	}
+
+	String eat () {
+		return "I want to eat";
+	}
+
+	void talk () {
+	}
+	
+	void learnJava () {	
+		
+	}
+}	
