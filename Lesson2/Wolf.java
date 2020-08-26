7d6df5f66f7d5e73ee849f8445eb9e2543120361
KaterinaBesp <KaterinaBesp@users.noreply.github.com>
2020-08-27 01:15:21 +0300

    Change

--- Lesson2/Wolf.java
+++ Lesson2/Wolf.java
@@ -1,28 +1,73 @@
 public class Wolf{
 
-	String name = "Polkan";
-	String sex = "Male";
-	String color = "gray";
-	float weight = 50f;
-	int age = 7;
+	private String name = "Polkan";
+	private String sex = "Male";
+	private String color = "gray";
+	private float weight = 50f;
+	private int age = 7;
 
-	boolean run () {
+	public String getName() {
+		return name;
+	}
+
+	void setNam (String name) {
+		this.name = name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	void setSex (String sex) {
+		this.sex = sex;
+	}	
+
+	public String getColor() {
+		return color;
+	}
+
+	void setColor(String color) {
+		this.color = color;
+	}	
+
+	public float getWeight() {
+		return weight;
+	}
+
+	void setWeight(float weight) {
+		this.weight = weight;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	void setAge(int age) {
+		if (age > 8) {
+			System.out.println("Incorrect age");
+		} else {
+		this.age = age;
+		}
+		
+	}
+
+	public boolean run() {
 		System.out.println("The wolf is running");
 		return true;
 	}
 
-	void sit () {
+	public void sit() {
 		System.out.println("The wolf is sit down");
 	}
 
-	String hunt () {
+	public String hunt() {
 		return "The wolf is hunt";
 	}
 
-	void howl () {
+	public void howl() {
 	}
 	
-	void go () {
+	public void go() {
 		System.out.println("The wolf goes");
 	}
 }	
--- Lesson2/WolfTest.java
+++ Lesson2/WolfTest.java
@@ -1,11 +1,15 @@
 public class WolfTest{
 	public static void main(String[] args) {
 		Wolf wolfOne = new Wolf();
-		System.out.println("age - " + wolfOne.age);
-		System.out.println("name -" + wolfOne.name);
-		System.out.println("name - " + wolfOne.color);
-		System.out.println(wolfOne.run());
+		System.out.println("name -" + wolfOne.getName());
+		System.out.println("sex -" + wolfOne.getSex());
+		System.out.println("color -" + wolfOne.getColor());
+		System.out.println("weight -" + wolfOne.getWeight());
+		System.out.println("age -" + wolfOne.getAge());
 		wolfOne.sit();
+		wolfOne.run();
 		wolfOne.hunt();
+		wolfOne.howl();
+		wolfOne.go();
 	}
 }
