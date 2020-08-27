d080f4b774dd2330677b5eac43a74933488e5ce7
KaterinaBesp <KaterinaBesp@users.noreply.github.com>
2020-08-28 01:30:03 +0300

    Change class Calculator,add CalculatorTest

--- Lesson2/CalculatorTest.java
+++ Lesson2/CalculatorTest.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
 public class CalculatorTest {
 	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		Scanner scan = new Scanner(System.in);
+		String answer;
+
+		do {
+			System.out.print("Enter the first number:");
+			calculator.setX(scan.nextInt());
+			scan.nextLine();
+			System.out.print("Select an operation: ");
+			calculator.setSign(scan.nextLine().charAt(0));
+			System.out.print("Enter the second number: ");
+			calculator.setY(scan.nextInt());
+			scan.nextLine();
+			calculator.calculate();
+			do {
+				System.out.print("Do you Want to continue? [Y/N]: ");
+				answer = scan.nextLine();
+			} while (!answer.equals("N") && !answer.equals("Y"));
+		} while (!answer.equals("N"));
 	}
+}
