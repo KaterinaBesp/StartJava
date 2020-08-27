d081f0e4233ec6cd3f1130ed52634c7a02fcc937
KaterinaBesp <KaterinaBesp@users.noreply.github.com>
2020-08-28 01:30:32 +0300

    change Calculator

---
+++ Lesson2/Calculator.java
@@ -0,0 +1,64 @@
+public class Calculator {
+
+	private	int x;
+	private	int y;
+	private	int result;
+	private	char sign;
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public char getSign() {
+		return sign;
+	}
+
+	public void setSign(char sign) {
+		this.sign = sign;
+	}
+
+	public int calculate() {
+		switch(sign){
+			case '+':
+				result = x + y;
+				System.out.println(" x + y = " + result);
+				break;
+			case '-':
+				result = x - y;
+				System.out.println(" x - y = " + result);
+			case  '*':
+				result = x * y;
+				System.out.println(" x * y = " + result);
+				break;
+			case '/':
+				result = x / y;
+				System.out.println(" x / y = " + result);
+				break;
+			case  '^':
+				result = x;
+				while (y > 1) { 
+					result *= x;
+					y--;
+				}
+				System.out.println(" x ^ y = " + result);
+				break;
+			case '%':
+				result = x % y;
+				System.out.println(" x % y = " + result);
+				break;
+		}	
+		return result;
+	}
+}
