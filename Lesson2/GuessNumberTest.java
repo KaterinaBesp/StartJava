f97bd74e315f824a41232e90490c47fe86b38a45
KaterinaBesp <KaterinaBesp@users.noreply.github.com>
2020-09-02 21:41:58 +0300

    add new class and test

---
+++ Lesson2/GuessNumberTest.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; 
+
+public class GuessNumberTest {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the first name");
+		Player player1 = new Player(scan.nextLine());
+		System.out.println("Enter the second name");
+		Player player2 = new Player(scan.nextLine());
+		GuessNumber game = new GuessNumber(player1, player2);
+		String answer;
+		do {
+			game.play();
+			do {
+				System.out.print("Do you Want to continue? [Y/N]: ");
+				answer = scan.nextLine();
+			} while (!answer.equals("N") && !answer.equals("Y"));
+		} while (!answer.equals("N"));
+	}
+}	
