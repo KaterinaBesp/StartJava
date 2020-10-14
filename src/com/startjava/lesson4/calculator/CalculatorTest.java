6a12dfa6767cfa4f26e6e93a866b98975302e706
KaterinaBesp <KaterinaBesp@users.noreply.github.com>
2020-10-14 19:22:30 +0300

    Change data for test

--- src/com/startjava/lesson4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson4/calculator/CalculatorTest.java
@@ -3,19 +3,19 @@ package com.startjava.lesson4.calculator;
 import java.util.Scanner;
 
 public class CalculatorTest {
-	public static void main(String[] args) {
-		Calculator calculator = new Calculator();
-		Scanner scan = new Scanner(System.in);
-		String answer;
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        Scanner scan = new Scanner(System.in);
+        String answer;
 
-		do {
-			System.out.print ("Enter math expression:");
-			calculator.setSrcMathExpression (scan.nextLine ());
-			calculator.calculate ();
-			do {
-				System.out.print("Do you Want to continue? [Y/N]: ");
-				answer = scan.nextLine();
-			} while (!answer.equals("N") && !answer.equals("Y"));
-		} while (!answer.equals("N"));
-	}
+        do {
+            System.out.print("Enter math expression:");
+            calculator.setSrcMathExpression(scan.nextLine());
+            calculator.calculate();
+            do {
+                System.out.print("Do you Want to continue? [Y/N]: ");
+                answer = scan.nextLine();
+            } while (!answer.equals("N") && !answer.equals("Y"));
+        } while (!answer.equals("N"));
+    }
 }
